eb56667257d02c53bb1b84a6c16435369c2048b8
Ammar Meshaal <157367715+Ammarrs@users.noreply.github.com>
2025-12-07 11:43:28 +0200

    gradient button wrapper

---
+++ lib/components/gradient_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class GradientButton extends StatelessWidget {
+  const GradientButton({
+    super.key,
+    required this.onPressed,
+    required this.chiled,
+  });
+
+  final VoidCallback onPressed;
+  final Widget chiled;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color(0xFF5B7C99), Color(0xFF4ECDC4)],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+        ),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        child: chiled,
+      ),
+    );
+  }
+}
